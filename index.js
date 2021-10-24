8bef9b3e6bb2a30808e1eb93acca40199a467aa5
Reema <rima.abouchakra.gms@gmail.com>
2021-10-24 14:39:39 +0300

    newest

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 function rima() {
     alert('rima w bas');
+    fetch(' https://gentle-castle-38614.herokuapp.com/data')
+        .then(response => response.json())
+        .then(json => console.log(json))
 }
